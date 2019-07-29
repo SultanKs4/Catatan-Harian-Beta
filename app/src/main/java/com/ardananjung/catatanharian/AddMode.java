141bed2aa0bd9209540b2019962968e8c06ebbc1
ardananjungkusuma <ardananjungkusuma@gmail.com>
2019-07-29 10:15:14 +0700

    update

--- app/src/main/java/com/ardananjung/catatanharian/AddMode.java
+++ app/src/main/java/com/ardananjung/catatanharian/AddMode.java
@@ -1,14 +1,25 @@
 package com.ardananjung.catatanharian;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.RoomDatabase;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.ardananjung.catatanharian.data.Aktifitas;
+import com.ardananjung.catatanharian.data.DatabaseTask;
+import com.ardananjung.catatanharian.data.DatabaseTaskEventListener;
+import com.ardananjung.catatanharian.data.catatanHarianDb;
 
 public class AddMode extends AppCompatActivity {
     EditText nama,keterangan,waktu;
+    Button buttonSelesai;
+    private Aktifitas currentAktifitas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +28,45 @@ public class AddMode extends AppCompatActivity {
         nama=findViewById(R.id.edtNama);
         keterangan=findViewById(R.id.edtKeterangan);
         waktu=findViewById(R.id.edtWaktu);
+        Button btn = findViewById(R.id.btnSelesai);
+        btn.setEnabled(false);
     }
+    private Aktifitas makeAktifitas(){
+        Aktifitas a = new Aktifitas();
+        a.namaKegiatan=this.nama.getText().toString();
+        a.keterangan=this.keterangan.getText().toString();
+        a.waktu=this.waktu.getText().toString();
+        return a;
+    }
+    private void syncData()
+    {
+        this.currentAktifitas.namaKegiatan = this.nama.getText().toString();
+        this.currentAktifitas.keterangan = this.keterangan.getText().toString();
+        this.currentAktifitas.waktu = this.waktu.getText().toString();
+    }
+
+    public void onBtnSave(View view){
+        new DatabaseTask(this, new DatabaseTaskEventListener() {
+
+            @Override
+            public Object runDatabaseOperation(RoomDatabase database, Object... params) {
+                Aktifitas aktifitas = (Aktifitas) params[0];
+                ((catatanHarianDb) database).aktifitasDao().insertAll(aktifitas);
+                return null;
+            }
+
+            @Override
+            public void onDatabaseOperationFinished(Object... results) {
+                new Handler().postDelayed(new Runnable(){
+                    @Override
+                    public void run(){
+                        Toast.makeText(getApplicationContext(), "Berhasil Ditambahkan", Toast.LENGTH_SHORT).show();
+                    }
+                }, 1000);
+            }
+        }).execute(this.makeAktifitas());
+    }
+
     public void kembali(View view) {
         Intent i = new Intent(AddMode.this, MainActivity.class);
         startActivity(i);
--- app/src/main/java/com/ardananjung/catatanharian/EditMode.java
+++ app/src/main/java/com/ardananjung/catatanharian/EditMode.java
@@ -11,13 +11,15 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.ardananjung.catatanharian.data.Aktifitas;
+import com.ardananjung.catatanharian.data.AktifitasDao;
+import com.ardananjung.catatanharian.data.AppDbProvider;
 import com.ardananjung.catatanharian.data.DatabaseTask;
 import com.ardananjung.catatanharian.data.DatabaseTaskEventListener;
 import com.ardananjung.catatanharian.data.catatanHarianDb;
 
 public class EditMode extends AppCompatActivity {
     EditText nama,keterangan,waktu;
-
+    private Aktifitas currentAktifitas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +35,13 @@ public class EditMode extends AppCompatActivity {
         a.keterangan=this.keterangan.getText().toString();
         a.waktu=this.waktu.getText().toString();
         return a;
+    }
+
+    private void syncData()
+    {
+        this.currentAktifitas.namaKegiatan = this.nama.getText().toString();
+        this.currentAktifitas.keterangan = this.keterangan.getText().toString();
+        this.currentAktifitas.waktu = this.waktu.getText().toString();
     }
 
     public void onBtnSave(View view){
@@ -61,8 +70,27 @@ public class EditMode extends AppCompatActivity {
             }
         }).execute(this.makeAktifitas());
     }
+
+    public void onBtnDelete(View view)
+    {
+        this.syncData();
+        AktifitasDao aktifitasDao = AppDbProvider.getInstance(this).aktifitasDao();
+        aktifitasDao.delete(this.currentAktifitas);
+        Toast.makeText(this, "Your data has been deleted..", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Lompati halaman sebelumnya //todo cek ini kalo force pindah
+        startActivity(intent);
+    }
+
     public void kembali(View view) {
         Intent i = new Intent(EditMode.this, MainActivity.class);
         startActivity(i);
+    }
+
+    public void updateData(View view) {
+        this.syncData();
+        AktifitasDao aktifitasDao = AppDbProvider.getInstance(this).aktifitasDao();
+        aktifitasDao.update(this.currentAktifitas);
+        Toast.makeText(this, "Your data has been updated!", Toast.LENGTH_SHORT).show();
     }
 }
